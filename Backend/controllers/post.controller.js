f3d163e9b2cf411ca74965015892accc1e54990b
GAURAV8520 <gauravdeshmukh897@gmail.com>
2024-06-30 17:26:05 +0530

    added comment functionality

--- Backend/controllers/post.controller.js
+++ Backend/controllers/post.controller.js
@@ -69,3 +69,36 @@ export const deletePost =async (req,res)=>{
         res.status(500).json({error:"Internal server error "})
     }
 }
+
+export const commmentOnPost = async (req,res)=>{
+    try {
+
+        const {text}= req.body;
+        const postId=req.params.id;
+        const userId=req.user._id;
+
+        if(!text){
+            return res.status(400).json({error:"Text field is required"})
+        }
+
+        const post=await Post.findById(postId);
+
+        if(!post){
+            return res.status(400).json({error:"Post not found"})
+        }
+
+        const comment ={text,user : userId};
+
+        post.comment.push(comment)
+
+        await post.save();
+
+        res.status(200).json(post);
+
+
+    } catch (error) {
+        console.log("Error in commentOnPost controller :",error);
+        return res.status(500).json({error:"Internal server error"})
+
+    }
+}
--- Backend/routes/post.routes.js
+++ Backend/routes/post.routes.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import { portectRoute } from '../middleware/portectRoute.js';
-import {createPost,deletePost} from '../controllers/post.controller.js'
+import {createPost,deletePost,commmentOnPost} from '../controllers/post.controller.js'
 
 const router=express.Router();
 
 router.post("/create",portectRoute,createPost)
 router.delete("/:id",portectRoute,deletePost)
 // router.post('/like',portectRoute,likeUnlikePost)
-// router.post('/comment',portectRoute,commmentOnPost)
+router.post("/comment/:id",portectRoute,commmentOnPost)
 
 
 export default router;
