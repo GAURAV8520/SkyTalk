26c1aaef5a36b9ec24ef93b82c60b99c17e6044b
GAURAV8520 <gauravdeshmukh897@gmail.com>
2024-06-30 17:50:38 +0530

    added liked functionality

--- Backend/controllers/post.controller.js
+++ Backend/controllers/post.controller.js
@@ -1,6 +1,8 @@
 import {User} from '../models/user.model.js'
 import {Post} from '../models/post.model.js'
 import {v2 as cloudinary} from 'cloudinary'
+import {Notification} from '../models/notification.model.js'
+
 
 export const createPost= async(req,res)=>{
     try {
@@ -102,3 +104,48 @@ export const commmentOnPost = async (req,res)=>{
 
     }
 }
+
+export const likeUnlikePost =async (req,res)=>{
+    try {
+        const userId=req.user._id;
+        const {id:postId}=req.params;
+
+        const post = await Post.findById(postId);
+
+        if(!post){
+            return res.status(400).json({error:"Post not found"})
+        }
+
+        const userLikedPost = post.likes.includes(userId);
+
+        if(userLikedPost){
+            //ullike the post
+
+            await Post.updateOne({_id:postId},{$pull:{likes:userId}})
+            res.status(200).json({message :"Post unliked successfully"})
+
+        }else{
+
+            //like post
+
+            post.likes.push(userId);
+            await post.save();
+
+            const notification =new Notification({
+                from:userId,
+                to:post.user,
+                type:"like"
+            })
+
+            await notification.save();
+
+            res.status(200).json({message:"Post liked successfully "})
+
+        }
+
+
+    } catch (error) {
+        console.log("Error in likedUnlikedPost controller :",error);
+        res.status(500).json({error:"Internal server error"})
+    }
+};
--- Backend/routes/post.routes.js
+++ Backend/routes/post.routes.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import { portectRoute } from '../middleware/portectRoute.js';
-import {createPost,deletePost,commmentOnPost} from '../controllers/post.controller.js'
+import {createPost,deletePost,commmentOnPost,likeUnlikePost} from '../controllers/post.controller.js'
 
 const router=express.Router();
 
 router.post("/create",portectRoute,createPost)
 router.delete("/:id",portectRoute,deletePost)
-// router.post('/like',portectRoute,likeUnlikePost)
+router.post('/like/:id',portectRoute,likeUnlikePost)
 router.post("/comment/:id",portectRoute,commmentOnPost)
 
 
