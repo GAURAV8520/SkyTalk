1374c14db846caf54334001ca0a7b2acf1c96f9a
GAURAV8520 <gauravdeshmukh897@gmail.com>
2024-07-03 17:33:24 +0530

    completed the ui frontend of project

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { Route,Routes } from "react-router-dom"
+import HomePage from "./Pages/home/HomePage"
+import SignUpPage from "./Pages/auth/signup/SignUpPage"
+import LoginPage from "./Pages/auth/login/LoginPage"
+import Sidebar from "./components/common/Sidebar"
+import RightPanel from "./components/common/RightPanel"
+import NotificationPage from "./Pages/notification/NotificationPage"
+import ProfilePage from "./Pages/profile/ProfilePage"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='flex max-w-6xl mx-auto'>
+
+      <Sidebar/>
+
+    <Routes>
+      <Route path='/' element={<HomePage/>} />
+      <Route path='login' element={<LoginPage/>}/>
+      <Route path='/signup' element={<SignUpPage/>} />
+      <Route path='/notifications' element={<NotificationPage/>} />
+      <Route path='/profile/:username' element={<ProfilePage/>} />
+
+    </Routes>
+
+    <RightPanel/>
+
+
+    </div>
   )
 }
 
