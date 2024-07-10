309145371b49bd5ca64164b71e487485a243c654
GAURAV8520 <gauravdeshmukh897@gmail.com>
2024-07-10 12:29:28 +0530

    code for post upload time

--- Frontend/src/components/common/Post.jsx
+++ Frontend/src/components/common/Post.jsx
@@ -8,6 +8,7 @@ import { Link } from "react-router-dom";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import {toast}  from 'react-hot-toast'
 import LoadingSpinner from './LoadingSpinner'
+import { formatPostDate } from "../../utils/date";
 
 const Post = ({ post }) => {
 
@@ -126,7 +127,7 @@ const Post = ({ post }) => {
 
 	const isMyPost = authUser._id===post.user._id;
 
-	const formattedDate = "1h";
+	const formattedDate = formatPostDate(post.createdAt);
 
 	// const isCommenting = false;
 
