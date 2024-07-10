e293262669a13c9ae0dc6b24645f2b708e664bef
GAURAV8520 <gauravdeshmukh897@gmail.com>
2024-07-10 12:52:25 +0530

    added the notification functionality in frontend

--- Frontend/src/Pages/notification/NotificationPage.jsx
+++ Frontend/src/Pages/notification/NotificationPage.jsx
@@ -3,34 +3,61 @@ import LoadingSpinner from "../../components/common/LoadingSpinner";
 
 import { IoSettingsOutline } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
-import { FaHeart } from "react-icons/fa6";
+import { FaHeart, FaThreads } from "react-icons/fa6";
+
+import {  useQuery, useMutation, useQueryClient} from "@tanstack/react-query";
+import toast from "react-hot-toast";
+
 
 const NotificationPage = () => {
-	const isLoading = false;
-	const notifications = [
-		{
-			_id: "1",
-			from: {
-				_id: "1",
-				username: "johndoe",
-				profileImg: "/avatars/boy2.png",
-			},
-			type: "follow",
-		},
-		{
-			_id: "2",
-			from: {
-				_id: "2",
-				username: "janedoe",
-				profileImg: "/avatars/girl1.png",
-			},
-			type: "like",
-		},
-	];
 
-	const deleteNotifications = () => {
-		alert("All notifications deleted");
-	};
+	const queryClient = useQueryClient();
+	
+	const {data:notifications,isLoading}=useQuery({
+		queryKey:["notifications"],
+		queryFn:async ()=>{
+			try {
+				const res = await fetch("/api/notifications");
+
+				const data= await res.json();
+
+				if(!res.ok){
+					throw new Error(data.error || "something went wrong");
+				}
+
+				return data;
+
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+	});
+
+	const {mutate:deleteNotifications}=useMutation({
+		mutationFn:async()=>{
+			try {
+				const res = await fetch("/api/notifications",{
+					method:"DELETE",
+
+				});
+				const data = await res.json();
+				if(!res.ok){
+					throw new Error(data.error || "something went wrong");
+				}
+				return data;
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+		onSuccess:()=>{
+			toast.success("Notification deleted successfully")
+			queryClient.invalidateQueries({queryKey:["notifications"]})
+		},
+		onError:(error)=>{
+			toast.error(error.message);
+		}
+	})
+	
 
 	return (
 		<>
