63ae63f34983dbf325942d4186a356e890632503
GAURAV8520 <gauravdeshmukh897@gmail.com>
2024-12-20 14:47:54 +0530

    solved the fronted ui errors

--- Frontend/src/Pages/auth/signup/SignUpPage.jsx
+++ Frontend/src/Pages/auth/signup/SignUpPage.jsx
@@ -59,7 +59,7 @@ const SignUpPage = () => {
 	
 
 	return (
-		<div className='max-w-screen-xl mx-auto flex h-screen px-10 bg-slate-600'>
+		<div className='max-w-screen-xl mx-auto flex h-screen px-10 '>
 			<div className='flex-1 hidden lg:flex items-center  justify-center'>
 				<XSvg className=' lg:w-2/3 fill-white' />
 			</div>
